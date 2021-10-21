5953258bc0b4c7e1412cd3146a05fcbacc5fbd62
simplg <81249731+simplg@users.noreply.github.com>
2021-10-21 11:05:55 +0200

    test

---
+++ tests/utils/test_compute.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+import math
+from app.lib.compute import sum
+
+
+@dataclass
+class SumItem():
+    x: int
+    y: int
+    result: int
+
+def test_sum():
+    tests = [
+        SumItem(0, 0, 0),
+        SumItem(-1, 1, 0),
+        SumItem(1, 1, 2),
+        SumItem(-1, -1, -2),
+        SumItem(-1, -1, -2)
+    ]
+    for test in tests:
+        assert sum(test.x, test.y) == test.result
